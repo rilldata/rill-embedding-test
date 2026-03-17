f4a5e7a1af876f5a8163a50756d52b4d66fa4c00
royendo <67675319+royendo@users.noreply.github.com>
2026-03-17 11:54:43 -0400

    change to .in

--- src/app/views/simple-iframe/page.tsx
+++ src/app/views/simple-iframe/page.tsx
@@ -10,7 +10,7 @@ const SimpleIframe = () => {
     /**
      * The full request would look like
      * 
-     * URL: https://admin.rilldata.io/v1/organizations/demo/projects/rill-openrtb-prog-ads/iframe
+     * URL: https://admin.rilldata.in/v1/organizations/demo/projects/rill-openrtb-prog-ads/iframe
      * Method: POST
      * headers: {
      *   Authorization: "Bearer ${rillServiceToken}"
@@ -52,7 +52,7 @@ const SimpleIframe = () => {
                 </li>
                 <li>
                     <a
-                        href="https://ui.rilldata.io/demo/rill-openrtb-prog-ads/explore/bids_explore"
+                        href="https://ui.rilldata.in/demo/rill-openrtb-prog-ads/explore/bids_explore"
                         className="text-indigo-600 underline hover:text-indigo-800"
                     >
                         Rill Dashboard
